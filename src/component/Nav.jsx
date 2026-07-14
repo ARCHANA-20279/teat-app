8841464a49e5a3863b6beb7bcba7bad22359d11f
ARCHANA-20279 <archana552m@gmail.com>
2026-07-14 23:10:12 +0530

    completed

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">team</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
